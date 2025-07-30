26350225a775d1c5c6036b4a284ad594eae4b5d4
Alexchê Hérica <alexcheherica@gmail.com>
2025-07-30 17:27:57 -0300

    Declarando um array

---
+++ js/03-arrays.js
@@ -0,0 +1,15 @@
+'use strict';
+
+//Declarando um array (vetor)
+
+let vocalistas = ["Ozzy", "Dio", "Lemmy", "David Gilmour"];
+
+//Acessando um determinado elemento atrvés do índice
+console.log(vocalistas[0]);
+
+//Se quisermos visualizar a estrutura do array inteiro:
+//console.log(vocalistas);
+
+//O David Gilmour está em turnê tocando Pink Floyd
+console.log(`O ${vocalistas[3]} está em turnê tocando Pink Floyd`);
+
