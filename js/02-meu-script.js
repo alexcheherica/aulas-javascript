07d93fb2aec05079a4b617efd32e686425de507e
Alexchê Hérica <alexcheherica@gmail.com>
2025-07-30 16:41:25 -0300

    Script Externo

---
+++ js/02-meu-script.js
@@ -0,0 +1,19 @@
+//Exercicio
+let uc = 2
+let competencia = "Front-End com JavaScript"
+
+
+console.log(`Estamos na UC${uc} e estamos estudando ${competencia}.`);
+
+// ERRADO!
+//console.log("Gosto "muito" de paçoca!");
+
+//CORRETO! ASPAS SIMPLES COMO COMANDO E DUPLAS COMO TEXTO
+console.log('Gosto "muito"de paçoca!');
+
+//CORRETO! DUPLAS SIMPLES COMO COMANDO E SIMPLES COMO TEXTO
+console.log("Gosto 'muito'de paçoca!");
+
+//CORRETO! Escapando as aspas como texto
+console.log("Gosto \"muito\" de paçoca!");
+
