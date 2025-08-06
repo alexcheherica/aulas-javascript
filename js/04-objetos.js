fc844f7e887fe0f6ac1bb479523b05ab289955ed
Alexchê Hérica <alexcheherica@gmail.com>
2025-08-06 16:50:53 -0300

    Objeto com Array

--- js/04-objetos.js
+++ js/04-objetos.js
@@ -15,3 +15,24 @@ console.log(pessoa);
 // Acessando as popriedades usando nomedDoObjeto.nomeDaPropriedade
 console.log(`O ${pessoa.nome} tem ${pessoa.idade} anos.`);
 
+// Exemplo 2:objeto com array
+console.log("\nExemplo 2\n");
+
+const livro= {
+    titulo: "O Senhor dos Anéis",
+    autor: "J.R.R Tolkien",
+    volumes: [
+        "A Sociedade do Anel",
+        "As Duas Torres",
+        "O Retorno do Rei"
+    ]
+};
+
+console.log(livro);
+console.log(livro.titulo);
+
+/*Para acessar algum dos volumes, primeiro passamos pela propriedade (volumes) usando 'ponto' e, chegando nela, usamos os 'colchetes' com o índice pois se trata de uma array */
+console.log(`Meu livro preferido é ${livro.volumes[1]}`);
+
+
+
