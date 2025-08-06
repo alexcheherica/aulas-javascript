963d473c241ef0fc4bbe33659157b4effe62831e
Alexchê Hérica <alexcheherica@gmail.com>
2025-08-06 16:33:31 -0300

    aprendendo sobre objetos no JS

---
+++ js/04-objetos.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// Exemplo 1: objeto com dados de uma pessoa
+console.log("Exemplo 1\n");
+
+const pessoa = {
+    // propriedade: valor
+    nome:"Chaves",
+    idade: 8,
+    cidade: "São Paulo",
+    estado: "SP"
+
+};
+console.log(pessoa);
+// Acessando as popriedades usando nomedDoObjeto.nomeDaPropriedade
+console.log(`O ${pessoa.nome} tem ${pessoa.idade} anos.`);
+
