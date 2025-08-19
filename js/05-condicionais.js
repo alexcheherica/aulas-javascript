13950178e137d972b2208e7f42ac7b9285c59b08
Alexchê Hérica <alexcheherica@gmail.com>
2025-08-19 14:36:30 -0300

    aula condicionais

---
+++ js/05-condicionais.js
@@ -0,0 +1,19 @@
+'use strict'
+
+console.log("Teste");
+
+/* Comandos Condicionais mais comuns:
+if -> SE
+else -> SENÃO
+*/
+
+console.log("Exemplo 1")
+//variavel
+let numero = 50;
+
+//SE numero for maior ou igual a 10
+if (numero >= 10) {
+    console.log("Se você está vendo essa mensagem é porque a condição é VERDADEIRA");
+    
+    
+}
