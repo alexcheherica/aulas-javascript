c8a90b12f635a04d5e1f779370da3fa5ecf1a18c
Alexchê Hérica <alexcheherica@gmail.com>
2025-08-19 14:56:11 -0300

    condicional composta

--- js/05-condicionais.js
+++ js/05-condicionais.js
@@ -7,13 +7,26 @@ if -> SE
 else -> SENÃO
 */
 
-console.log("Exemplo 1")
+console.log("Exemplo 1");
 //variavel
 let numero = 50;
 
 //SE numero for maior ou igual a 10
 if (numero >= 10) {
     console.log("Se você está vendo essa mensagem é porque a condição é VERDADEIRA");
-    
-    
+       
+}
+
+console.log("\nExemplo 2")
+
+let aluno = "Ozzy";
+let idade = 30;
+
+/*Lógica: verificar se o aluno é maior ou menor de idade. */
+
+if (idade < 18){
+    console.log("Você é menor de idade!");     
+} 
+else{
+    console.log("Você é maior de idade!");
 }
