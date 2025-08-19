cdbbf6bacd0301b8a1b8560bc0fa158e218ae60c
Alexchê Hérica <alexcheherica@gmail.com>
2025-08-19 15:21:27 -0300

    Exercício condicionais

--- js/05-condicionais.js
+++ js/05-condicionais.js
@@ -30,3 +30,32 @@ if (idade < 18){
 else{
     console.log("Você é maior de idade!");
 }
+
+//Exercicio
+/*1. Crie duas variáveis conforme a seguir:
+ 
+- Nota 1 (contendo um valor de 0 a 10)
+- Nota 2 (contendo outro valor de 0 a 10)
+ 
+2. Crie uma variável chamada "Média" que receberá o valor CALCULADO da média das duas notas informadas. 
+DICA: você deve SOMAR as duas notas e DEPOIS dividir por 2.
+ 
+3. Programe uma condicional que verifique o valor da média calculada. Se a média for maior/igual a 7, mostre "aprovado". 
+Caso contrário, mostre "reprovado". */
+
+
+console.log("\nExercicio");
+
+let NotaUm = 7;
+let NotaDois = 9;
+let Media = ( NotaUm + NotaDois )/2;
+
+if (Media >= 7) {
+
+    console.log("Aprovado");  
+}
+else{
+    console.log("Reprovado");
+    
+}
+
