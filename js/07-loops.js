d4011c637f689763af04311692d8fa1db42b36d9
Alexchê Hérica <alexcheherica@gmail.com>
2025-08-20 14:11:50 -0300

    loops while

--- js/07-loops.js
+++ js/07-loops.js
@@ -6,4 +6,15 @@ São comandos que, baseados em alguma condição lógica, são programados para 
 Comandos mais tradicionais:while(ENQUANTO) e for (PARA)
 
 Normalmente, os loops são controlados através de uma variálel (let) contadora.
-*/ 
+*/
+
+console.log("Exemplo 1: WHILE (enquanto)");
+
+let contador = 1;
+while (contador <= 5) {
+    console.log("Valor do contador:"+contador);
+    contador++; //incremento
+    // contador = contador + 1;
+       
+}
+
