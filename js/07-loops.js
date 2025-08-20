4ae721f77b3d3196f6a8f1765eaf7331675f5a65
Alexchê Hérica <alexcheherica@gmail.com>
2025-08-20 14:16:22 -0300

    troca do nome da variável

--- js/07-loops.js
+++ js/07-loops.js
@@ -5,16 +5,18 @@ São comandos que, baseados em alguma condição lógica, são programados para 
 
 Comandos mais tradicionais:while(ENQUANTO) e for (PARA)
 
-Normalmente, os loops são controlados através de uma variálel (let) contadora.
+Normalmente, os loops são controlados através de uma variálel (let) ia.Esta variável é comumente chamada de "i", "j", "k" ou qualquer outra letra se necessário.
 */
 
 console.log("Exemplo 1: WHILE (enquanto)");
 
-let contador = 1;
-while (contador <= 5) {
-    console.log("Valor do contador:"+contador);
-    contador++; //incremento
-    // contador = contador + 1;
+let i = 1;
+while (i <= 5) {
+    console.log("Valor do i:"+i);
+    i++; //incremento
+    // i = i + 1;
        
 }
+
+
 
