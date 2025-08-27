e295f7fc213c43feade09092177374404c2dea39
Alexchê Hérica <alexcheherica@gmail.com>
2025-08-27 14:01:38 -0300

    querySelectorAll

--- js/09-manipulando-dom-e-eventos.js
+++ js/09-manipulando-dom-e-eventos.js
@@ -11,3 +11,11 @@ console.log(exemplo01);
 console.log(mensagem01);
 console.log(pagina);
 
+/*Para selecionar vários elementos, use querySelectorAll.
+O resultado é uma espécie de array de elementos(NodeList).
+*/
+
+const paragrafo = document.querySelectorAll("p");
+console.log(paragrafo);
+
+
